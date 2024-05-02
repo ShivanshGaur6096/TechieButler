2437e9069d898c539f3f13399b233ec4b2190534
Shivansh Gaur <shivansh.gaur@blackngreen.com>
2024-05-02 18:03:18 +0530

    Fix: Added code documentation

--- TechieButler/Utils/Extensions/Int+Extension.swift
+++ TechieButler/Utils/Extensions/Int+Extension.swift
@@ -7,8 +7,14 @@
 
 import Foundation
 
-extension Int {
-    
+public extension Int {
+    /**
+     Returns the ordinal representation of the integer value.
+
+     For example, it converts `1` to `"1st"`, `2` to `"2nd"`, `3` to `"3rd"`, `4` to `"4th"`, and so on.
+     
+     - Returns: The ordinal representation of the integer value as a `String`.
+     */
     var ordinalRepresentation: String {
         let suffixes = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"]
         switch self % 100 {
